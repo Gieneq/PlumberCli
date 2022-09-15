0b8b24ef6aff683dedbaa19d83f643bf1bb8e26b
Gieneq <piotr.adamczyk.gda@gmail.com>
2022-09-15 09:33:44 +0200

    Added Preset builder

--- plumber.py
+++ plumber.py
@@ -3,7 +3,7 @@ from board import Point, Board
 from render import Renderer
 
 class Game:
-    def __init__(self):
+    def __init__(self, presets:Presets):
         self.board = Board()
 
     def start(self):
